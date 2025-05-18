fc511ab1bd685302b751a6ece9f8bf80daebe4e5
barty32 <barty12matyas@gmail.com>
2025-05-19 01:31:07 +0200

    Refactored errors

--- src/errors.ts
+++ src/errors.ts
@@ -1,31 +1,16 @@
 
-
-export class ConnectionError extends Error {
+class BaseError extends Error {
 	constructor(message: string) {
 		super(message);
-		this.name = "ConnectionError";
+		//automatically set the name of the error
+		this.name = this.constructor.name;
 	}
 }
 
-export class ZebraPrinterLanguageUnknownError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = "ZebraPrinterLanguageUnknownError";
-	}
-}
-
-export class ZebraPrinterParseError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = "ZebraPrinterParseError";
-	}
-}
-
-export class DiscoveryError extends Error {
-	constructor(message: string) {
-		super(message);
-		this.name = "DiscoveryError";
-	}
-}
-
-
+export class ConnectionError extends BaseError {}
+export class NotALinkOsPrinterError extends BaseError {}
+export class ZebraPrinterLanguageUnknownError extends BaseError {}
+export class ZebraIllegalArgumentError extends BaseError {}
+export class ZebraPrinterParseError extends BaseError {}
+export class DiscoveryError extends BaseError {}
+export class IOError extends BaseError {}
